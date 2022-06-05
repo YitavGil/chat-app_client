ca2e96d8b71908445b6229229e8b0a0f96af88bc
YitavGil <yitavga@yahoo.com>
2022-06-05 08:46:20 +0300

    footer complete

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
-import { Logo } from '../logo';
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Logo } from "../logo";
 
 const FooterContainer = styled.div`
   //min-height: 24em;
@@ -17,21 +19,171 @@ const FooterContainer = styled.div`
   `};
 `;
 
+const InnerContainer = styled.div`
+  ${tw`
+    flex
+    w-full
+    h-full
+    max-w-screen-2xl
+    flex-wrap
+  `};
+`;
+
 const AboutContainer = styled.div`
   ${tw`
     flex
     flex-col
   `}
-`
+`;
+
+const AboutText = styled.p`
+  ${tw`
+    text-white
+    text-sm
+    font-normal
+    max-w-xs
+    leading-5
+    mt-2
+  `};
+`;
+
+const SectionContainer = styled.div`
+  ${tw`
+    w-full
+    md:w-auto
+    flex
+    flex-col
+    mr-2
+    md:mr-16
+    pl-10
+    pr-10
+    md:pl-3
+    md:pr-3
+    mt-7
+    md:mt-0
+  `};
+`;
+
+const LinksList = styled.ul`
+  ${tw`
+    outline-none
+    list-none
+    flex
+    flex-col
+  `};
+`;
+
+const ListItem = styled.li`
+  ${tw`
+    mb-3
+  `};
+  & > a {
+    ${tw`
+      text-sm
+    text-white
+      transition-all
+      hover:text-gray-200
+    `};
+  }
+`;
+
+const HeaderTitle = styled.h3`
+  ${tw`
+    text-2xl
+    font-bold
+    text-white
+    mb-3
+  `};
+`;
+
+const HorizontalContainer = styled.div`
+  ${tw`
+    flex
+    items-center
+  `};
+`;
+
+const BlueIcon = styled.span`
+  ${tw`
+    w-9
+    h-9
+    rounded-full
+    bg-blue-500
+    flex
+    items-center
+    justify-center
+    text-white
+    text-base
+    mr-2
+  `};
+`;
+
+const SmallText = styled.h6`
+  ${tw`
+    text-sm
+    text-white
+  `};
+`;
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <AboutContainer>
-        <Logo color='white' />
-      </AboutContainer>
+      <InnerContainer>
+        <AboutContainer>
+          <Logo color="white" />
+          <AboutText>
+            Lighthouse is an home sublet network company located all over Israel
+          </AboutText>
+        </AboutContainer>
+        <SectionContainer>
+          <HeaderTitle>Website</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="/#">Home</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">About Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Services</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Blog</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="/#">FAQ</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Contact Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Support</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Privacy Policy</a>
+            </ListItem>
+            <ListItem>
+              <a href="/#">Terms & Conditions</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Call Now</HeaderTitle>
+          <HorizontalContainer>
+            <BlueIcon>
+              <FontAwesomeIcon icon={faPhoneAlt} />
+            </BlueIcon>
+            <SmallText>+972 52-666-666</SmallText>
+          </HorizontalContainer>
+        </SectionContainer>
+      </InnerContainer>
     </FooterContainer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
